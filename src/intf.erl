c6a18f9dd84794d9d46abf2aacc6fa64c4da3530
Alexandre Tamborrino <tamborrino.alexandre@gmail.com>
2012-09-10 15:55:57 +0200

    add of interfces

---
+++ src/intf.erl
@@ -0,0 +1,65 @@
+%% Author: atamborrino
+%% Created: 10 Sep 2012
+%% Description: TODO: Add description to intf
+-module(intf).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([]).
+
+%%
+%% API Functions
+%%
+
+new() ->
+    [].
+
+add(Name,Ref,Pid,Intf) ->
+    [{Name,Ref,Pid} | Intf].
+
+remove(Name,Intf) ->
+    lists:delete(Name, Intf).
+
+lookup(Name,Intf) ->
+    case lists:keyfind(Name, 1, Intf) of
+        {_,_,Pid} ->
+            {ok,Pid};
+        false ->
+            notfound
+    end.
+
+ref(Name,Intf) ->
+    case lists:keyfind(Name, 1, Intf) of
+        {_,Ref,_} ->
+            {ok,Ref};
+        false ->
+            notfound
+    end.
+
+name(Ref,Intf) ->
+    case lists:keyfind(Ref, 2, Intf) of
+        {Name,_,_} ->
+            {ok,Name};
+        false ->
+            notfound
+    end.
+
+list(Intf) ->
+    [Name || {Name,_,_} <- Intf].
+
+broadcast(Message,Intf) ->
+    lists:foreach(fun({_,_,Pid})->
+                          Pid ! Message
+                          end, Intf).
+    
+
+
+%%
+%% Local Functions
+%%
+
